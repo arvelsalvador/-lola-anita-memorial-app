2b9c30a288989fa242639fd0c51bfb1350f2f71a
Arvel Salvador <arvelsalvador@gmail.com>
2026-08-12 11:27:55 +0800

    feat: Integrate LanguageProvider and update app structure

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:nita/core/constants/app_constants.dart';
 import 'package:nita/core/constants/app_routes.dart';
+import 'package:nita/core/localization/language_provider.dart';
 import 'package:nita/features/home/home_page.dart';
 import 'package:nita/features/splash/splash_page.dart';
 
@@ -9,20 +11,23 @@ class LolaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'In Loving Memory',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.cream,
-        fontFamily: 'Georgia',
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.rose),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (_) => LanguageProvider(),
+      child: MaterialApp(
+        title: 'In Loving Memory',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: AppColors.cream,
+          fontFamily: 'Georgia',
+          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.rose),
+          useMaterial3: true,
+        ),
+        initialRoute: AppRoutes.splash,
+        routes: {
+          AppRoutes.splash: (_) => const SplashPage(),
+          AppRoutes.home: (_) => const HomePage(),
+        },
       ),
-      initialRoute: AppRoutes.splash,
-      routes: {
-        AppRoutes.splash: (_) => const SplashPage(),
-        AppRoutes.home: (_) => const HomePage(),
-      },
     );
   }
 }
